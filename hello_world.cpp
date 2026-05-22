708cee67922fb858ee710ad21804a0a17a79314f
ponomarevmiron <ponomarevmiron@gmail.com>
2026-05-22 18:45:03 +0000

    apply clang-format Mozilla style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <string>
 
-int main() {
-    // Спрашиваем имя!!!
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
-    // Печатаем приветствие!!!
-    std::cout << "Hello world from " << name << std::endl;
-    return 0;
+int
+main()
+{
+  // Спрашиваем имя!!!
+  std::string name;
+  std::cout << "Enter your name: ";
+  std::cin >> name;
+  // Печатаем приветствие!!!
+  std::cout << "Hello world from " << name << std::endl;
+  return 0;
 }
