40f38ec628b2ce7e840d5fa3944f88dd3b5377f0
ponomarevmiron <ponomarevmiron@gmail.com>
2026-05-22 19:17:53 +0000

    Create conflicting comments in master

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,11 +2,11 @@
 #include <string>
 
 int main() {
-    // Запрашиваем имя пользователя
+    // Спрашиваем имя!!!
     std::string name;
     std::cout << "Enter your name: ";
     std::cin >> name;
-    // Выводим приветствие
+    // Печатаем приветствие!!!
     std::cout << "Hello world from " << name << std::endl;
     return 0;
 }
